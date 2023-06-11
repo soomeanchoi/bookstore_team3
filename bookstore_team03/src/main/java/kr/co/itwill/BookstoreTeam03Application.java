ea104d9272e19bdfa61a7f3b6fcf7f523d6b32d7
DESKTOP-DB2GBMB\sumin <suminchoi0123@gmail.com>
2023-06-12 00:16:39 +0900

    [Test]프로젝트 생성

---
+++ bookstore_team03/src/main/java/kr/co/itwill/BookstoreTeam03Application.java
@@ -0,0 +1,13 @@
+package kr.co.itwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreTeam03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreTeam03Application.class, args);
+	}
+
+}
