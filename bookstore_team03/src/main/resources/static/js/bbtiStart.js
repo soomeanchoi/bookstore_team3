1ece591009001c9f8de26b9b0839414a7701e48b
kokiuk <kgukid38@naver.com>
2023-06-25 18:09:09 +0900

    답변 리스트에 저장

--- bookstore_team03/src/main/resources/static/js/bbtiStart.js
+++ bookstore_team03/src/main/resources/static/js/bbtiStart.js
@@ -1,6 +1,7 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 const result = document.querySelector("#result");
+const selected = [];
 
 var que;
 var choose_A;
@@ -8,34 +9,20 @@ var choose_B;
 
 var endPoint;
 
-
-
 $.ajax({
     url: "/research/kakao1",
     type: "GET",
     success: function (data) {
-
-
         que = data.que;
         choose_A = data.choose_A;
         choose_B = data.choose_B;
 
-        // alert(que.length);
         endPoint = que.length;
-        // alert(endPoint);
-        // alert("hi")
-
-        // alert(que.toString());
-        // alert(choose_A.toString());
-        // alert(choose_B.toString());
-
     },
     error: function (xhr, status, error) {
-        //요청에 실패한 경우 에러 처리 수행
         console.error("AJAX 요청 실패:", status, error);
     }
-})
-
+});
 
 function addAnswer(answerText, qIdx) {
     var a = document.querySelector('.answerBox');
@@ -48,23 +35,25 @@ function addAnswer(answerText, qIdx) {
 
     a.appendChild(answer);
     answer.innerHTML = answerText;
-    answer.addEventListener("click", function(){
+    answer.addEventListener("click", function() {
         var children = document.querySelectorAll('.que');
-        for (let i =0; i < children.length; i ++){
+        for (let i = 0; i < children.length; i++) {
             children[i].disabled = true;
             children[i].style.WebkitAnimation = "fadeOut 0.5s";
             children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(() => {
-            for (let i =0; i < children.length; i ++){
+            for (let i = 0; i < children.length; i++) {
                 children[i].style.display = 'none';
             }
+            var selectedAnswer = answerText;
+            selected.push(selectedAnswer);
             goNext(++qIdx);
-        },450)
+        }, 450);
     }, false);
 }
 
-function goResult(){
+function goResult() {
     qna.style.WebkitAnimation = "fadeOut 1s";
     qna.style.animation = "fadeOut 1s";
     setTimeout(() => {
@@ -73,22 +62,24 @@ function goResult(){
         setTimeout(() => {
             qna.style.display = "none";
             result.style.display = "block";
-        }, 450)})
-    }
+        }, 450);
+    }, 500);
+    alert(selected.join(", "));
+}
 
 function goNext(qIdx) {
-    if (qIdx === endPoint){
+    if (qIdx === endPoint) {
         goResult();
         return;
     }
     var q = document.querySelector(".qBox");
     q.innerHTML = que[qIdx];
-    addAnswer(choose_A[qIdx] ,qIdx);
+    addAnswer(choose_A[qIdx], qIdx);
     addAnswer(choose_B[qIdx], qIdx);
     var status = document.querySelector('.statusBar');
-    status.style.width = (100/endPoint) * (qIdx+1) + '%';
-
+    status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
 }
+
 function begin() {
     main.style.WebkitAnimation = "fadeOut 1s";
     main.style.animation = "fadeOut 1s";
@@ -98,9 +89,8 @@ function begin() {
         setTimeout(() => {
             main.style.display = "none";
             qna.style.display = "block";
-        },450)
+        }, 450);
         let qIdx = 0;
         goNext(qIdx);
-    },500)
-
+    }, 500);
 }
