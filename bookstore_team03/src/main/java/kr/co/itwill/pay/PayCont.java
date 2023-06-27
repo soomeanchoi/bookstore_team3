ce17d00843507d1aec7087b0bcd88f24394f67c1
kokiuk <kgukid38@naver.com>
2023-06-27 11:46:26 +0900

    border 데이터 조회까지 오나료

--- bookstore_team03/src/main/java/kr/co/itwill/pay/PayCont.java
+++ bookstore_team03/src/main/java/kr/co/itwill/pay/PayCont.java
@@ -1,17 +1,25 @@
 package kr.co.itwill.pay;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/pay")
 public class PayCont {
+
+    @Autowired
+    PayDAO payDAO;
+
+
 
     public PayCont() {
         System.out.println("----PayCont()객체 생성됨");
@@ -25,7 +33,12 @@ public class PayCont {
         System.out.println("borderNo = " + borderNo);
 
         //넘겨줄 데이터들을 담아줄 Map 생성
-        Map<String, Object> data = new HashMap<>();
+        List<Map<String, Object>> data = new ArrayList<>();
+
+        //넘겨줄 데이터들 조회
+        System.out.println(payDAO.selectData(borderNo));
+
+        data = payDAO.selectData(borderNo);
 
 
 
--- bookstore_team03/src/main/java/kr/co/itwill/pay/PayDAO.java
+++ bookstore_team03/src/main/java/kr/co/itwill/pay/PayDAO.java
@@ -1,11 +1,25 @@
 package kr.co.itwill.pay;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PayDAO {
 
+    @Autowired
+    SqlSession sqlSession;
+
     public PayDAO() {
         System.out.println("----PointDAO()객체 생성됨");
     }
+
+    public List<Map<String,Object>> selectData(String border_no){
+        return sqlSession.selectList("pay.list", border_no);
+    }
+
+
 }
